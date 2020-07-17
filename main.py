fdf011c73ebd1bad5231e856bacd8ad639c099b7
desmondBull88 <66146449+desmondBull88@users.noreply.github.com>
2020-07-16 18:23:03 -0700

    Larges changes, addtion of many screens

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-
+import json
+from datetime import datetime
 
 Builder.load_file('layout.kv')
 
@@ -10,18 +11,45 @@ class LoginScreen(Screen):
     def sign_up(self):
         self.manager.current = 'sign_up_screen'
 
+    def login(self, user, pword):
+        with open('users.json') as file:
+            users = json.load(file)
+        if user in users and users[user]['password'] == pword:
+            self.manager.current = 'login_screen_success'
+        else:
+            self.ids.login_wrong.text = 'The username nor the password exists!'
+
 
 class RootWidget(ScreenManager):
     pass
 
 
+class SignUpScreen(Screen):
+    def add_user(self, uname, pword):
+        with open('users.json') as file:
+            users = json.load(file)
+        users[uname] = {"username": uname,
+                        "password": pword, "created": datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
+        with open("users.json", 'w') as file:
+            json.dump(users, file)
+        self.manager.current = 'sign_up_screen_success'
+
+
+class SignUpScreenSuccess(Screen):
+    def return_login(self):
+        self.manager.transition.direction = 'right'
+        self.manager.current = 'login_screen'
+
+
+class LoginScreenSuccess(Screen):
+    def log_out(self):
+        self.manager.transition.direction = 'right'
+        self.manager.current = "login_screen"
+
+
 class MainApp(App):
     def build(self):
         return RootWidget()
-
-
-class SignUpScreen(Screen):
-    pass
 
 
 if __name__ == '__main__':
